8fc81bb283275fa4cac506766e6a18f83a70ed5b
Jorge Matos <jisapjisap@gmail.com>
2026-08-04 16:10:31 +0200

    feat: create AppContext and provider to manage global user authentication state

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,9 +1,13 @@
 import { createContext, useContext, useState, useEffect } from "react"
 import api from "../api/api";
+import toast from "react-hot-toast";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const AppContext = createContext(undefined);
 
 export const AppProvider = ({ children }) => {
+
+  const navigate = useNavigate();
 
   // Auth States
   const [user, setUser] = useState(null);
@@ -23,10 +27,39 @@ export const AppProvider = ({ children }) => {
 
   useEffect(() => {
     checkSessions()
-  }, []);
+  }, []); // No se pone chekSession en las dependencias para evitar un bucle infinito. checkSession -> setUser -> AppProvider se renderiza de nuevo -> nuevo checkSession -> ... infinito.
+
+  const login = async (email, password) => {
+    try {
+      const { data } = await api.post("/api/auth/login", { email, password });
+      setUser(data.user);
+      toast.success(`Welcome back!, ${data.user.name}`);
+      navigate("/");
+    } catch (error) {
+      console.error("Login failed", error);
+      const errMsg = error?.response?.data?.error || "Login failed. Invalid email or password. Please try again.";
+      toast.error(errMsg);
+      throw new Error(errMsg);
+    }
+  }
+
+  const register = async (name, email, password) => {
+    try {
+      const { data } = await api.post("/api/auth/register", { name, email, password });
+      setUser(data.user);
+      toast.success("Account created successfully");
+      navigate("/");
+    } catch (error) {
+      console.error("Registration failed", error);
+      const errMsg = error?.response?.data?.error || "Registration failed.";
+      toast.error(errMsg);
+      throw new Error(errMsg);
+    }
+  }
+
 
   return (
-    <AppContext.Provider value={{ user, loadingUser }}>
+    <AppContext.Provider value={{ user, loadingUser, login, register }}>
       {children}
     </AppContext.Provider>
   )
